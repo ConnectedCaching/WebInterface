146c5c6770c94ab7b6aeb9b7cc774949a5036780
Leo Schweizer <leonhard.schweizer@gmail.com>
2013-11-03 12:57:50 +0100

    FastForward integration

--- project/Build.scala
+++ project/Build.scala
@@ -13,18 +13,18 @@ object ApplicationBuild extends Build {
 		"org.webjars" % "bootstrap" % "3.0.0",
 		"org.webjars" % "extjs" % "4.2.1.883",
 		"com.feth" %%  "play-authenticate" % "0.5.0-SNAPSHOT",
-		"org.reactivemongo" %% "play2-reactivemongo" % "0.10.0-SNAPSHOT"
+		"org.reactivemongo" %% "play2-reactivemongo" % "0.10.0-SNAPSHOT",
+		"play" %% "fastforward" % "0.1-SNAPSHOT"
 	)
 
 	val main = play.Project(appName, appVersion, appDependencies).settings(
-
+		resolvers += "FastForward (snapshots)" at "http://repo.acira.net/snapshots/",
 		resolvers += Resolver.url("play-easymail (release)", url("http://joscha.github.com/play-easymail/repo/releases/"))(Resolver.ivyStylePatterns),
 		resolvers += Resolver.url("play-easymail (snapshot)", url("http://joscha.github.com/play-easymail/repo/snapshots/"))(Resolver.ivyStylePatterns),
 		resolvers += Resolver.url("play-authenticate (release)", url("http://joscha.github.com/play-authenticate/repo/releases/"))(Resolver.ivyStylePatterns),
 		resolvers += Resolver.url("play-authenticate (snapshot)", url("http://joscha.github.com/play-authenticate/repo/snapshots/"))(Resolver.ivyStylePatterns),
 		// required for Reactive Mongo Snapshots
 		resolvers += "Sonatype Snapshots" at "http://oss.sonatype.org/content/repositories/snapshots/"
-
 	)
 
 }
